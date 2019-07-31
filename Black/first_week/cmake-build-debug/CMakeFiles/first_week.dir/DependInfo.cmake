0820c20808a6d6fcb2cf53179a0de58490bba5b1
momsspaghettti <ivan.samoilov.1999@yandex.ru>
2019-08-01 00:07:56 +0500

    Started first week with Clion

---
+++ Black/first_week/cmake-build-debug/CMakeFiles/first_week.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ivan/Рабочий стол/coursera-c-plus-plus-modern-development/Black/first_week/main.cpp" "/home/ivan/Рабочий стол/coursera-c-plus-plus-modern-development/Black/first_week/cmake-build-debug/CMakeFiles/first_week.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Black/first_week/cmake-build-debug/CMakeFiles/first_week.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_week.dir/main.cpp.o"
+  "first_week.pdb"
+  "first_week"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_week.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
